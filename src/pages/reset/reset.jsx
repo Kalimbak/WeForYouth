c17d347ffa6f6eb2f6908c5281f6bad3b988de62
mbarndouka <mmbarndouka@gmail.com>
2022-07-28 12:09:56 +0200

    Changes Translate

---
+++ src/pages/reset/reset.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Navbar from '../../components/Navbar/navbar' 
+import './reset.css'
+
+function reset() {
+  return (
+    <div className='resetPage'>
+        <div className=''>
+            <Navbar />
+        </div>
+    </div>
+  )
+}
+
+export default reset
