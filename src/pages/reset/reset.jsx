13028beebc266a3f325a26c1b931d59fb8cb5f58
mbarndouka <mmbarndouka@gmail.com>
2022-07-28 19:19:14 +0200

    reset-page, i finish the reset page already

--- src/pages/reset/reset.jsx
+++ src/pages/reset/reset.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import Navbar from '../../components/Navbar/navbar' 
+import Visibility from '../../assets/visibility_off'
 import './reset.css'
 
 function reset() {
@@ -7,6 +8,28 @@ function reset() {
     <div className='resetPage'>
         <div className=''>
             <Navbar />
+        </div>
+        <div className='BodyReset'>
+          <p>Reset your Password</p>
+          <div className='ResetP'>
+            <div className='place'>
+              <input type="text" placeholder='E-mail' />
+            </div>
+            <div className='place'>
+              <input type="text"  placeholder='Type Codes From the Email'/>
+            </div>
+            <div className='place'>
+              <input type="text"  placeholder='New  password'/>
+              <Visibility />
+            </div>
+            <div className='place'>
+              <input type="text"  placeholder='Confirm Password'/>
+              <Visibility />
+            </div>
+            <div className=''>
+              <button className='btn'> Send</button>
+            </div>
+          </div>
         </div>
     </div>
   )
